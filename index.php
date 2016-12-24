617459caf1479da0ab142ca5c55048852b48ba99
Shabeer Hussain <shabeershussain@gmail.com>
2016-12-24 09:34:51 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,62 +1,46 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html>
 <head>
-	<meta name="description" content="Shabeer S Hussain, iOS Engineer" />
-	<meta name="keywords" content="iOS, Swift, Obj-c, Objective-c, Objective c, iPhone, iPad, Apps, Mobile, app" />
-	<meta name="author" content="Shabeer S Hussain" />
-	<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-
-	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-	<link rel="icon" href="/favicon.ico" type="image/x-icon">
-
-	<link rel="stylesheet" type="text/css" href="/css/default.css">
-	<link rel="stylesheet" type="text/css" href="/css/typography.css">
-	<title> Shabeer Hussain</title>
+	<meta content="Shabeer S Hussain, iOS Engineer" name="description">
+	<meta content="iOS, Swift, Obj-c, Objective-c, Objective c, iPhone, iPad, Apps, Mobile, app" name="keywords">
+	<meta content="Shabeer Hussain" name="author">
+	<meta content="text/html; charset=utf-8" http-equiv="content-type">
+	<link href="/favicon.ico" rel="shortcut icon" type="image/x-icon">
+	<link href="/favicon.ico" rel="icon" type="image/x-icon">
+	<link href="/css/default.css" rel="stylesheet" type="text/css">
+	<link href="/css/typography.css" rel="stylesheet" type="text/css">
+	<title>Shabeer Hussain</title>
 </head>
 <body>
-<div id="container">
-      <div id="nav">
-   <div        
-	      <ul>
-<li><a href="index.php">About</a></li>
-<li><a href="portfolio.php">Portfolio</a></li>
-<li><a href="education.php">Education</a></li>
-</ul>
-
-
-
-
-</div>
-        </div>
-<div id="content"><p>
-<p>
-I am a London based iOS developer specialising in native iOS app development. I also work as an iOS trainer, teaching industry developers who specialise in other languages the art of iOS! When I am not teaching I create beautiful and smooth Apps for hire.</p>
-<p>For freelance projects such as iPhone and iPad apps please contact me at <b><a href="mailto:ShabeerSHussain@gmail.com?subject=Development%20project%20inquiry">ShabeerSHussain@gmail.com</a></b> to discuss how I can meet your development needs.</p>
-<p>Feel free to browse my <a href="portfolio.php">portfolio</a> to view some projects I have worked on along with some random experiments.</p>
-<p><a href="https://dl.dropboxusercontent.com/u/63208053/Shabeer%20Developer%20CV.pdf">Download CV</a></p>
-
-<p>
-<h1> Delivered</h1>
-<h2> Now available on the App Store </h2>
-
-I have just launched my latest iOS app Delivered, an app which aims to help delivery men and all courier personel complete their day-to-day tasks. Check out the trailer below or <a href="http://tinyurl.com/deliveredapp">download it now</a> for free on the App Store
-</p>
-
-   
-			<p>
-            <center>
-               <!-- <video width="500" height="281" controls poster="http://www.delivered.desertmonkey.co.uk/images/videoThumbnail.png">
+	<div id="container">
+		<div id="nav">
+			<ul>
+				<li><a href="index.php">About</a></li>
+				<li><a href="portfolio.php">Portfolio</a></li>
+				<li><a href="education.php">Education</a></li>
+			</ul>
+		</div>
+		<div id="content">
+			<p></p>
+			<p>I am a London based iOS developer specialising in native iOS app development. I also work as an iOS trainer, teaching industry developers who specialise in other languages the art of iOS! When I am not teaching I create beautiful and smooth Apps for hire.</p>
+			<p>For freelance projects such as iPhone and iPad apps please contact me at <b><a href="mailto:ShabeerSHussain@gmail.com?subject=Development%20project%20inquiry">ShabeerSHussain@gmail.com</a></b> to discuss how I can meet your development needs.</p>
+			<p>Feel free to browse my <a href="portfolio.php">portfolio</a> to view some projects I have worked on along with some random experiments.</p>
+			<p><a href="https://dl.dropboxusercontent.com/u/63208053/Shabeer%20Developer%20CV.pdf">Download CV</a></p>
+			<p></p>
+			<h1>Delivered</h1>
+			<h2>Now available on the App Store</h2>I have just launched my latest iOS app Delivered, an app which aims to help delivery men and all courier personel complete their day-to-day tasks. Check out the trailer below or <a href="http://tinyurl.com/deliveredapp">download it now</a> for free on the App Store
+			<p></p>
+			<p></p>
+			<center>
+				<!-- <video width="500" height="281" controls poster="http://www.delivered.desertmonkey.co.uk/images/videoThumbnail.png">
                   <source src="http://www.delivered.desertmonkey.co.uk/video/trailer.mov" type="video/mp4">
                   Your browser does not support the video tag.
                 </video> -->
-                <iframe width="560" height="315" src="//www.youtube.com/embed/DPLfgV0BdCs" frameborder="0" allowfullscreen></iframe>
-                </center>
-            </p>
-            <p>
-            For more information on Delivered or to join the Delivered mailing list to be the first to hear about the latest developments visit the <a href="http://delivered.desertmonkey.co.uk"> Delivered home page </a>. 
-
-        </div>
-    </div>
+				<iframe allowfullscreen frameborder="0" height="315" src="//www.youtube.com/embed/DPLfgV0BdCs" width="560"></iframe>
+			</center>
+			<p></p>
+			<p>For more information on Delivered or to join the Delivered mailing list to be the first to hear about the latest developments visit the <a href="http://delivered.desertmonkey.co.uk">Delivered home page</a> .</p>
+		</div>
+	</div>
 </body>
-
 </html>
